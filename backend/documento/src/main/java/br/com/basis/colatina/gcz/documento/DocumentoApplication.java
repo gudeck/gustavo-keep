3ddc719bf6efc9bde2a80747135ec8209f6911d6
gudeck <guzuc@outlook.com>
2021-06-29 09:31:49 -0300

    configurando o projeto

---
+++ backend/documento/src/main/java/br/com/basis/colatina/gcz/documento/DocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.basis.colatina.gcz.documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentoApplication.class, args);
+    }
+
+}
