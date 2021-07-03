3dbf7d09200b8fd141d7a57b966f7558d80b20fc
gudeck <guzuc@outlook.com>
2021-07-03 15:23:41 -0300

    configurando minio

---
+++ backend/documento/src/main/java/br/com/basis/colatina/gcz/documento/config/MinioConfiguration.java
@@ -0,0 +1,42 @@
+package br.com.basis.colatina.gcz.documento.config;
+
+import br.com.basis.colatina.gcz.documento.config.propertie.ApplicationProperties;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class MinioConfiguration {
+
+    private final ApplicationProperties applicationProperties;
+
+    @SneakyThrows
+    private boolean bucketExists(MinioClient minioClient, String bucketName) {
+        return minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+    }
+
+    @SneakyThrows
+    private void createBucket(MinioClient minioClient) {
+        var bucket = applicationProperties.getMinio().getBucket();
+        if (!bucketExists(minioClient, bucket)) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+        }
+    }
+
+    @Bean
+    public MinioClient minioClient() {
+        var minioProperties = applicationProperties.getMinio();
+        var minioClient = MinioClient.builder()
+                .endpoint(minioProperties.getUrl())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+        createBucket(minioClient);
+        return minioClient;
+    }
+
+}
---
+++ backend/documento/src/main/java/br/com/basis/colatina/gcz/documento/config/propertie/MinioProperties.java
@@ -0,0 +1,13 @@
+package br.com.basis.colatina.gcz.documento.config.propertie;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MinioProperties {
+    private String url;
+    private String bucket;
+    private String accessKey;
+    private String secretKey;
+}
