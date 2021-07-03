6f4b0399f29671a2bc457d0a397869ad092a4afb
gudeck <guzuc@outlook.com>
2021-07-03 18:32:13 -0300

    configurando elasticsearch

---
+++ backend/keep/src/main/java/br/com/basis/colatina/gcz/keep/config/propertie/ElasticsearchProperties.java
@@ -0,0 +1,12 @@
+package br.com.basis.colatina.gcz.keep.config.propertie;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ElasticsearchProperties {
+
+    private String url;
+
+}
