c27650ce06e95b36ff5d1a626dff8b473ce38087
gudeck <guzuc@outlook.com>
2021-07-03 15:22:47 -0300

    configurando feign

--- backend/keep/src/main/java/br/com/basis/colatina/gcz/keep/KeepApplication.java
+++ backend/keep/src/main/java/br/com/basis/colatina/gcz/keep/KeepApplication.java
@@ -2,7 +2,9 @@ package br.com.basis.colatina.gcz.keep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class KeepApplication {
 
